260ebb2a6825cb9f1fed216de79a2f880bdbf182
Yury Glushkov <mail@glushkov.us>
2018-06-05 17:39:34 +0300

    IsWalletAsync extension method tests added

---
+++ tests/Lykke.Service.GenericEthereumIntegration.Common.Tests/Core/Services/Extensions/BlockchainServiceExtensionsTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using Lykke.Service.GenericEthereumIntegration.Common.Core.Services.Extensions;
+using Lykke.Service.GenericEthereumIntegration.Common.Core.Services.Interfaces;
+using Lykke.Service.GenericEthereumIntegration.TDK;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Lykke.Service.GenericEthereumIntegration.Common.Tests.Core.Services.Extensions
+{
+    [TestClass]
+    public class BlockchainServiceExtensionsTests
+    {
+        [TestMethod]
+        public async Task IsWalletAsync__GetCodeAsyncReturnsEmptyResult__TrueReturned()
+        {
+            var service = new Mock<IBlockchainService>();
+
+            service
+                .Setup(x => x.GetCodeAsync(It.IsAny<string>()))
+                .ReturnsAsync("0x");
+            
+            Assert.IsTrue(await service.Object.IsWalletAsync(TestValues.ValidAddress1));
+            
+            service
+                .Verify(x => x.GetCodeAsync(It.IsAny<string>()), Times.Once);
+            
+            service
+                .VerifyNoOtherCalls();
+        }
+        
+        [TestMethod]
+        public async Task IsWalletAsync__GetCodeAsyncReturnsNonEmptyResult__FalseReturned()
+        {
+            var service = new Mock<IBlockchainService>();
+
+            service
+                .Setup(x => x.GetCodeAsync(It.IsAny<string>()))
+                .ReturnsAsync("0xAe");
+            
+            Assert.IsFalse(await service.Object.IsWalletAsync(TestValues.ValidAddress1));
+            
+            service
+                .Verify(x => x.GetCodeAsync(It.IsAny<string>()), Times.Once);
+            
+            service
+                .VerifyNoOtherCalls();
+        }
+    }
+}
